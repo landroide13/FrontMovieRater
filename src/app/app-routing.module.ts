5b2429bec6ce07a71485f1d8f611e40a2a86fade
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-05 16:43:23 +1200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'auth', component:AuthComponent},
+  {path:'movies', component:MainComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
